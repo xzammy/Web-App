386c9714e5a54401fab8b587af6fdead75f59a1e
Luo <xwhalex@icloud.com>
2019-07-15 09:35:43 +0800

    Animation added to sidebar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { Box, Button, Heading, Grommet } from 'grommet';
-import { Hpe , Notification } from'grommet-icons';
+import { Box, Button, Collapsible, Heading, Grommet } from 'grommet';
+import { Hpe, Notification } from 'grommet-icons';
 
 const theme = {
-  global:{
-    colors:{
-      brand:'#FFFFFF'
+  global: {
+    colors: {
+      brand: '#FFFFFF'
     },
-    font:{
+    font: {
       family: 'Noto Serif',
       size: '14px',
       height: '20px',
@@ -18,31 +18,53 @@ const theme = {
 
 const AppBar = (props) => (
   <Box
-    tag= 'header'
-    direction ='row'
-    align= 'center'
+    tag='header'
+    direction='row'
+    align='center'
     justify='between'
     background='brand'
-    pad={{ left:'medium', right:'small', vertical: 'small'}}
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
     elevation='medium'
     {...props}
-    />
+  />
 );
 
 class App extends Component {
+  state = {
+    showSidebar: false,
+  }
   render() {
+    const { showSidebar } = this.state;
     return (
       <Grommet theme={theme} full>
         <Box fill>
-        <AppBar>
-         <Heading level='3' margin='none'> Sudden Impact </Heading>
-         <Button icon={<Notification />} onClick={() =>{}}/>
-        </AppBar>
-        <Box Direction='row' flex overflow={{horizontal:'hidden'}}>
-      <Box flex align='center' justify ='center'>
-        Some text here
-         </Box> 
-        </Box>
+          <AppBar>
+            <Heading level='3' margin='none'> Sudden Impact </Heading>
+
+            <Button
+              icon={<Notification />}
+              onClick={() => this.setState(prevState => ({ showSidebar: !prevState.showSidebar }))}
+            />
+          </AppBar>
+          <Box Direction='row' flex overflow={{ horizontal: 'hidden' }}>
+            <Box flex align='center' justify='center'>
+              Some text here
+         </Box>
+
+            <Collapsible direction="horizontal" open={showSidebar}>
+              <Box
+                flex
+                width='medium'
+                background='light-2'
+                elevation='small'
+                align='center'
+                justify='center'
+              >
+                sidebar
+          </Box>
+          </Collapsible>
+           
+          </Box>
         </Box>
       </Grommet>
     );
